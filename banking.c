3cda1625ba65015b531a00adcd65a9c67ce6ae7d
mwenda <brianmwenda255@gmail.com>
2023-01-04 18:32:32 +0300

    write promnt keys

--- banking.c
+++ banking.c
@@ -17,5 +17,10 @@ int main()
     char filename[50],phone[50],password[50],phone2[50];
     FILE *fp,*fptr;
 	int opt,choice;
+    int amount;
+	char cont = 'y';
+	printf("\nWhat do you want to do?");
+	printf("\n\n1.Register your account");
+	printf("\n2.Login to your account");
 
 }
