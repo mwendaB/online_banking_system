48ab4816ce4a18f1820f1ffe467172da1f41a55e
mwenda <brianmwenda255@gmail.com>
2023-01-04 19:14:03 +0300

    create main user for online banking

--- OnlineBanking.c
+++ OnlineBanking.c
@@ -8,3 +8,17 @@ struct user {
     char password[50];
     float balance;
 };
+int main(){
+    struct user usr,usr1;
+    FILE *fp;
+    char filename[50],phone[50],pword[50];
+    int opt,choice;
+    char cont = 'y';
+    float amount;
+
+    printf("\nWhat do you want to do?");
+    printf("\n\n1. Register an account");
+    printf("\n2. Login to an account");
+
+    printf("\n\nYour choice:\t");
+    scanf("%d",&opt);
